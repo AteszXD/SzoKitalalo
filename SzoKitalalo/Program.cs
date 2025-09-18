20dbb5872f085b167a75897ff9e3812bfef44df9
AteszXD <keria@kkszki.hu>
2025-09-18 12:16:20 +0200

    A játék alapja működik, lehet nyerni és veszteni

--- SzoKitalalo/Program.cs
+++ SzoKitalalo/Program.cs
@@ -10,9 +10,62 @@ namespace SzoKitalalo
     {
         static void Main(string[] args)
         {
+            Console.Title = "Szókitaláló";
+
+            List<string> guessedLetters = new List<string>();
             string[] words = { "első", "alma", "kukac", "vizibicikli" };
+            byte remainingLives = 10;
+            string guessedLetter = "";
+            string wordState = "";
+
             Random random = new Random();
-            Console.WriteLine(words[random.Next(words.Length)]);
+            string wordToGuess = words[random.Next(words.Length)];
+
+            do
+            {
+                wordState = "";
+                foreach (char letter in wordToGuess)
+                {
+                    if (guessedLetters.Contains(Convert.ToString(letter)))
+                    {
+                        wordState += letter;
+                    } 
+                    else
+                    {
+                        wordState += '_';
+                    }        
+                }
+                Console.WriteLine(wordState);
+
+                if (!wordState.Contains("_"))
+                {
+                    Console.WriteLine("Gratulálok! Kitaláltad a szót!");
+                    break;
+                }
+
+                Console.Write($"{remainingLives} életed van még...");
+                Console.Write("\nKérem tippeljen betűt: ");
+                guessedLetter = Console.ReadLine();
+
+                if (guessedLetters.Contains(guessedLetter))
+                {
+                    Console.WriteLine("Ezt a betűt már tippelted");
+                }
+                else
+                {
+                    guessedLetters.Add(guessedLetter.ToLower());
+                    if (!wordToGuess.Contains(guessedLetter))
+                    {
+                        remainingLives -= 1;
+                    }
+                }
+
+            } while (remainingLives > 0);
+
+            if (remainingLives == 0)
+            {
+                Console.WriteLine("Kifogytál az életekből, vége a játéknak!");
+            }
         }
     }
 }
