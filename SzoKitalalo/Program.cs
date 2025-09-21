79343a5717d5ef1fc2a44b8fb067200822ffa13d
AteszXD <keria@kkszki.hu>
2025-09-21 19:18:05 +0200

    Refaktoráltam külön függvénybe

--- SzoKitalalo/Program.cs
+++ SzoKitalalo/Program.cs
@@ -27,6 +27,66 @@ namespace SzoKitalalo
 
             do
             {
+                DifficultySelector();
+            } while (!selected);
+
+            #region Game-Loop
+            Console.ResetColor(); // Enélkül a "Szakértő" mód piros lenne, nem tudom miért
+            do
+            {
+                wordState = showCurrentProgress();
+
+                if (!wordState.Contains("_"))
+                {
+                    Console.WriteLine("Gratulálok! Kitaláltad a szót!");
+                    break;
+                }
+
+                Console.Write($"{remainingLives} életed van még...");
+                Console.Write("\nKérem tippeljen betűt: ");
+                guessedLetter = Console.ReadLine();
+
+                if (guessedLetters.Contains(guessedLetter))
+                {
+                    Console.WriteLine("Ezt a betűt már tippelted");
+                }
+                else
+                {
+                    guessedLetters.Add(guessedLetter.ToLower());
+                    if (!wordToGuess.Contains(guessedLetter.ToLower()))
+                    {
+                        remainingLives -= 1;
+
+                        if (remainingLives == 0)
+                        {
+                            Console.WriteLine("Kifogytál az életekből, vége a játéknak!");
+                        }
+                    }
+                }
+            } while (remainingLives > 0);
+            #endregion
+
+            string showCurrentProgress()
+            {
+                string wordState = "";
+                foreach (char letter in wordToGuess)
+                {
+                    if (guessedLetters.Contains(Convert.ToString(letter)))
+                    {
+                        wordState += letter;
+                    }
+                    else
+                    {
+                        wordState += '_';
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine(wordState);
+                return wordState;
+            }
+
+            void DifficultySelector()
+            {
                 Console.Clear();
                 Console.ResetColor();
                 Console.WriteLine("Kérem válasszon nehézséget: ");
@@ -92,62 +152,7 @@ namespace SzoKitalalo
                         remainingLives = 3;
                         break;
                 }
-            } while (!selected);
-
-            #region Game-Loop
-            Console.ResetColor(); // Enélkül a "Szakértő" mód piros lenne, nem tudom miért
-            do
-            {
-                wordState = showCurrentProgress();
-
-                if (!wordState.Contains("_"))
-                {
-                    Console.WriteLine("Gratulálok! Kitaláltad a szót!");
-                    break;
-                }
-
-                Console.Write($"{remainingLives} életed van még...");
-                Console.Write("\nKérem tippeljen betűt: ");
-                guessedLetter = Console.ReadLine();
-
-                if (guessedLetters.Contains(guessedLetter))
-                {
-                    Console.WriteLine("Ezt a betűt már tippelted");
-                }
-                else
-                {
-                    guessedLetters.Add(guessedLetter.ToLower());
-                    if (!wordToGuess.Contains(guessedLetter.ToLower()))
-                    {
-                        remainingLives -= 1;
-
-                        if (remainingLives == 0)
-                        {
-                            Console.WriteLine("Kifogytál az életekből, vége a játéknak!");
-                        }
-                    }
-                }
-            } while (remainingLives > 0);
-            #endregion
-
-        string showCurrentProgress()
-        {
-            string wordState = "";
-            foreach (char letter in wordToGuess)
-            {
-                if (guessedLetters.Contains(Convert.ToString(letter)))
-                {
-                    wordState += letter;
-                }
-                else
-                {
-                    wordState += '_';
-                }
             }
-            Console.Clear();
-            Console.WriteLine(wordState);
-            return wordState;
-        }
         }
     }
 }
